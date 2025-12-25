65302a87bf98656504a83b22bcd2844418efa0de
callhub816-creator <callhub816@gmail.com>
2025-12-25 09:12:58 +0530

    Update index.ts

--- data/blogs/index.ts
+++ data/blogs/index.ts
@@ -7,6 +7,6 @@ import carRentalScamsIndia2026 from './carRentalScamsIndia2026';
 export const blogPosts = [
   delhiJaipurRoadTrip,
   carRentalScamsIndia,
+  longDriveSafetyChecklist,
   carRentalScamsIndia2026,
-  longDriveSafetyChecklist
 ];
